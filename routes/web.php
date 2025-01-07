587077a1db804a975e74c4c9944917c9e11d5665
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-07 15:59:00 +0700

    feat: add about page route
    Added a new route for the about page in routes/web.php.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// add new route, /about, that will return about view, when user access /about
 Route::get('/about', function () {
     return view('about');
+});
+
+// menampilkan data secara langsung melalui route
+Route::get('/hello', function () {
+    // return data dalam bentuk json, dengan key message dan value Hello World
+    // return response()->json([
+    //     'message' => 'Hello World'
+    // ]);
+
+    // atau bisa juga menggunakan array, lalu di return
+    $dataArray = [
+        'message' => 'Hello World'
+    ];
+
+    // atau bisa juga menggunakan helper function response()->json,
+    // yang akan mengubah array menjadi json, 
+    // dan juga memberikan status code 200 -- ini kelebihannya
+    return response()->json($dataArray, 200);
+
+    // ajaibnya, laravel akan mengubah array menjadi json secara otomatis
+    // return $dataArray;
 });
